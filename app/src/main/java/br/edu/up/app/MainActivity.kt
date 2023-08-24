703c49174df6cdc007b09b1c79c4f1298ecde151
mthhenri <mathsilvaesilva@gmail.com>
2023-08-24 11:40:36 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/up/app/MainActivity.kt
@@ -0,0 +1,22 @@
+package br.edu.up.app
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun showSecond(view: View) {
+        supportFragmentManager.commit {
+            replace<SecondFragment>(R.id.fragmentContainerView4)
+        }
+    }
+}
---
+++ app/src/main/java/br/edu/up/app/SecondFragment.kt
@@ -0,0 +1,7 @@
+package br.edu.up.app
+
+import androidx.fragment.app.Fragment
+
+class SecondFragment : Fragment(R.layout.fragment_second) {
+
+}
